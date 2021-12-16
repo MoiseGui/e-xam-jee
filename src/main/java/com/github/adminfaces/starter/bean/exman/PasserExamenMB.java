2ce574e14d373182d1dc8c18d591dec8c8297703
imaneEmi <imane.elaimani@gmail.com>
2021-12-16 19:01:29 +0100

    Added examen-pass view

--- src/main/java/com/github/adminfaces/starter/bean/exman/PasserExamenMB.java
+++ src/main/java/com/github/adminfaces/starter/bean/exman/PasserExamenMB.java
@@ -25,109 +25,121 @@ import static com.github.adminfaces.template.util.Assert.has;
 @Named
 @SessionScoped
 public class PasserExamenMB implements Serializable {
-    String id;
-    String examenId;
-    Examen examen;
-    String ownerName="";
+	String id;
+	String examenId;
+	Examen examen;
+	String ownerName = "";
 
-    @Inject
-    private AdminConfig adminConfig;
-    @Inject
-    ExamenService examenService;
+	@Inject
+	private AdminConfig adminConfig;
+	@Inject
+	ExamenService examenService;
 
-    @Inject
-    UserService userService;
+	@Inject
+	UserService userService;
 
-    public void init() throws IOException {
-        if (Faces.isAjaxRequest()) {
-            return;
-        }
+	public void init() throws IOException {
+		if (Faces.isAjaxRequest()) {
+			return;
+		}
 //        if(examen == null) {
-            if (has(id)) examen = examenService.findByID(id);
-            else Faces.redirect(adminConfig.getIndexPage());
+		if (has(id))
+			examen = examenService.findByID(id);
+		else
+			Faces.redirect(adminConfig.getIndexPage());
 //        }
 
-        if(examen != null) {
-            User user = userService.findUserById(examen.getOwner());
+		if (examen != null) {
+			User user = userService.findUserById(examen.getOwner());
 
-            if(user != null) {
-                ownerName = user.getPrenom() + " " + user.getNom();
-            }
-        }
+			if (user != null) {
+				ownerName = user.getPrenom() + " " + user.getNom();
+			}
+		}
 
-    }
+	}
 
-    public void findExamen() throws IOException {
-        examen = examenService.findByID(examenId);
-        if(examen == null) {
-            throw new BusinessException("Examen introuvable " + new ObjectId(examenId));
-        }
-        else{
+	public void findExamen() throws IOException {
+		examen = examenService.findByID(examenId);
+		if (examen == null) {
+			throw new BusinessException("Examen introuvable " + new ObjectId(examenId));
+		} else {
 //            addDetailMessage("Examen trouvé " + examenId);
 //            Faces.getExternalContext().getFlash().setKeepMessages(true);
-            Faces.redirect("examen.jsf?id=" + examenId);
-        }
-    }
+			Faces.redirect("examen.jsf?id=" + examenId);
+		}
+	}
 
-    public String formatDate(Date date){
-        if(date != null){
-            DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm");
-            return dateFormat.format(date);
-        }
-        return "Date inexistante";
-    }
+	public void passerExam() throws IOException {
+		examen = examenService.findByID(examenId);
+		if (examen == null) {
+			throw new BusinessException("Examen introuvable " + new ObjectId(examenId));
+		} else {
+		Faces.redirect("examen-pass.jsf?id=" + examenId);
+		}
+	}
 
-    // retourne la durée en heure:minutes de l'examen
-    public String getExamDuration(){
-        if(examen != null){
-            // calcul de la durée en minutes
-            long duree = examen.getDateFin().getTime() - examen.getDateDebut().getTime();
-            long dureeEnMinutes = duree / (60 * 1000);
-            // calcul de l'heure
-            long heure = dureeEnMinutes / 60;
-            // calcul des minutes
-            long minutes = dureeEnMinutes % 60;
-            if(heure == 0) return minutes + " minutes";
-            String result = heure + "h ";
-            if(minutes > 0) result += minutes + "min";
-            return result;
-        }
-        return "";
-    }
+	public String formatDate(Date date) {
+		if (date != null) {
+			DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+			return dateFormat.format(date);
+		}
+		return "Date inexistante";
+	}
 
-    public boolean canPasssExam(){
-        return examen.getDateDebut().before(new Date()) && examen.getDateFin().after(new Date());
-    }
+	// retourne la durée en heure:minutes de l'examen
+	public String getExamDuration() {
+		if (examen != null) {
+			// calcul de la durée en minutes
+			long duree = examen.getDateFin().getTime() - examen.getDateDebut().getTime();
+			long dureeEnMinutes = duree / (60 * 1000);
+			// calcul de l'heure
+			long heure = dureeEnMinutes / 60;
+			// calcul des minutes
+			long minutes = dureeEnMinutes % 60;
+			if (heure == 0)
+				return minutes + " minutes";
+			String result = heure + "h ";
+			if (minutes > 0)
+				result += minutes + "min";
+			return result;
+		}
+		return "";
+	}
 
-    public String getId() {
-        return id;
-    }
+	public boolean canPasssExam() {
+		return examen.getDateDebut().before(new Date()) && examen.getDateFin().after(new Date());
+	}
 
-    public void setId(String id) {
-        this.id = id;
-    }
+	public String getId() {
+		return id;
+	}
 
-    public String getExamenId() {
-        return examenId;
-    }
+	public void setId(String id) {
+		this.id = id;
+	}
 
-    public void setExamenId(String examenId) {
-        this.examenId = examenId;
-    }
+	public String getExamenId() {
+		return examenId;
+	}
 
-    public Examen getExamen() {
-        return examen;
-    }
+	public void setExamenId(String examenId) {
+		this.examenId = examenId;
+	}
 
-    public void setExamen(Examen examen) {
-        this.examen = examen;
-    }
+	public Examen getExamen() {
+		return examen;
+	}
 
-    public String getOwnerName() {
-        return ownerName;
-    }
+	public void setExamen(Examen examen) {
+		this.examen = examen;
+	}
 
-    public void setOwnerName(String ownerName) {
-        this.ownerName = ownerName;
-    }
+	public String getOwnerName() {
+		return ownerName;
+	}
+
+	public void setOwnerName(String ownerName) {
+		this.ownerName = ownerName;
+	}
 }
